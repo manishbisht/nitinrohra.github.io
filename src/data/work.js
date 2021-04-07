ed4c79e2a578cc17b4d9b073ee92d263bc0c1ba7
kabeerchaturvedi <kbrchaturvedi@gmail.com>
2021-04-07 11:01:36 +0530

    first push

--- src/data/work.js
+++ src/data/work.js
@@ -13,4 +13,18 @@ export default [
         image:
             "https://images.unsplash.com/photo-1554080353-a576cf803bda?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
     },
+    {
+        id: "3",
+        title: "sasas",
+        description: "sasas",
+        image:
+            "https://images.unsplash.com/photo-1554080353-a576cf803bda?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+    },
+    {
+        id: "4",
+        title: "sasas",
+        description: "sasas",
+        image:
+            "https://images.unsplash.com/photo-1554080353-a576cf803bda?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+    },
 ];
--- src/pages/work/index.js
+++ src/pages/work/index.js
@@ -1,13 +1,24 @@
 import React from "react";
 import workData from "../../data/work";
+import '../work/styles.css';
 
 const Work = () => {
     const renderWorkSection = () =>
         workData.map((workItem) => (
-            <div>
-                <div>{workItem.title}</div>
-                <div>{workItem.description}</div>
-                <img src={workItem.image} />
+            <div className="maindiv">
+                <div className="column">
+                    <div className="cardx">
+                    <img src={workItem.image} className="res-img"/>
+                    <div className="middle">
+                        <div className="text">
+                        <div>{workItem.title}</div>
+                    <div>{workItem.description}</div>
+                        </div>
+                    </div>
+
+
+                    </div>
+                </div>
             </div>
         ));
 
