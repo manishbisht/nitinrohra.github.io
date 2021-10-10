e0d2dcf9259e32abc3cb9d22912161dd60dd6e7f
Nitin Rohra <iamnrohra@gmail.com>
2021-10-10 23:28:01 +0530

    work-commit-first

--- src/data/work.js
+++ src/data/work.js
@@ -1,14 +1,196 @@
 export default [
     {
         id: "0f6280b0-240d-4b35-8d81-2e29585becf7",
-        title: "Noor - A Fashion Campaign",
-        slug: "noor-a-fashion-campaign",
+        title: "Noor II",
+        slug: "noor-a-fashion-campaign-two",
         type: "Ad. Campaign",
         role: "DOP",
         details: [
             {
                 type: "VIDEO",
                 videoId: "cFA6Gx3n-hU",
+            },
+        ],
+    },
+    {
+        id: "421ce365-ad3a-4d2a-bdbe-bbb65d3d97ba",
+        title: "Noor I",
+        slug: "noor-a-fashion-campaign-one",
+        type: "Ad. Campaign",
+        role: "DOP",
+        details: [
+            {
+                type: "VIDEO",
+                videoId: "aCb5V0NZBaI",
+            },
+        ],
+    },
+    {
+        id: "03e7beaa-ee52-48e5-b76d-c999155f4004",
+        title: "Shaam-e-Awadh",
+        slug: "shaam-e-awadh",
+        type: "Event Highlights",
+        role: "DOP",
+        details: [
+            {
+                type: "VIDEO",
+                videoId: "MI0PqYu10jI",
+            },
+        ],
+    },
+    {
+        id: "a5c6c1e3-a4c7-411e-a626-64f5c86324b9",
+        title: "Sach Keh Raha Hai Cover",
+        slug: "sach-keh-raha-hai-cover",
+        type: "Music Video",
+        role: "DOP",
+        details: [
+            {
+                type: "VIDEO",
+                videoId: "MTUzwE_HVSo",
+            },
+        ],
+    },
+    {
+        id: "682a6a28-4ddf-4e63-804f-1314709e2354",
+        title: "Women of 2035",
+        slug: "women-of-2035",
+        type: "Campaign Film",
+        role: "DOP",
+        details: [
+            {
+                type: "VIDEO",
+                videoId: "AMsQM1wXAMk",
+            },
+        ],
+    },
+    {
+        id: "54c06573-378f-4c02-a7bb-aef67011fdbf",
+        title: "WCMT x ISOBAR",
+        slug: "wcmt-x-isobar",
+        type: "Ad. Campaign",
+        role: "DOP",
+        details: [
+            {
+                type: "VIDEO",
+                videoId: "1t7VMGUZuxg",
+            },
+        ],
+    },
+    {
+        id: "3d8394ae-1c4a-4336-bebf-93cafd363b91",
+        title: "The Legend of Jungle",
+        slug: "the-legend-of-jungle",
+        type: "Event Highlights",
+        role: "DOP",
+        details: [
+            {
+                type: "VIDEO",
+                videoId: "Cwmus-2CfZA",
+            },
+        ],
+    },
+    {
+        id: "25809712-ff2d-437f-968c-8ffdffcf5f67",
+        title: "Bubbles & Bowties",
+        slug: "bubbles-and-bowties",
+        type: "Event Highlights",
+        role: "DOP",
+        details: [
+            {
+                type: "VIDEO",
+                videoId: "ao-XiHjzc3s",
+            },
+        ],
+    },
+    {
+        id: "544e889e-1820-403e-840b-4f567e172750",
+        title: "Bindu",
+        slug: "bindu",
+        type: "Short Film",
+        role: "Assistant Director",
+        details: [
+            {
+                type: "VIDEO",
+                videoId: "9SNHbp_uJ2k",
+            },
+        ],
+    },
+    {
+        id: "41f58500-eee6-4aa6-a04d-3b0a3881b873",
+        title: "Phadphadlal",
+        slug: "phadphadlal",
+        type: "Ad. Film",
+        role: "DOP",
+        details: [
+            {
+                type: "VIDEO",
+                videoId: "AVsH5UOvisA",
+            },
+        ],
+    },
+    {
+        id: "aa9739d7-6883-41b8-a811-9352260e23a7",
+        title: "Nut Cracker",
+        slug: "nut-cracker",
+        type: "Event Highlights",
+        role: "DOP",
+        details: [
+            {
+                type: "VIDEO",
+                videoId: "fp7SmYdqTQA",
+            },
+        ],
+    },
+    {
+        id: "d8a6b57c-cd11-435f-887d-cbab6bab8219",
+        title: "Breaking Bride",
+        slug: "breaking-bride",
+        type: "Short Film",
+        role: "DOP",
+        details: [
+            {
+                type: "VIDEO",
+                videoId: "c9PlEhF1ZpM",
+            },
+        ],
+    },
+    {
+        id: "ee942925-64a4-4481-a8dd-667685e33884",
+        title: "Showbuzz",
+        slug: "showbuzz",
+        type: "Ad. Campaign",
+        role: "DOP",
+        details: [
+            {
+                type: "VIDEO",
+                videoId: "3r-bMF8YQ_M",
+            },
+        ],
+    },
+    {
+        id: "5e30c2a5-99b6-4a75-9836-6ae6df66dbe0",
+        title: "Dream Girl",
+        slug: "dream-girl",
+        type: "Short Film",
+        role: "II Unit Cine.",
+        details: [
+            {
+                type: "VIDEO",
+                videoId: "FXhd5z9MsfU",
+            },
+        ],
+    },
+    {
+        id: "418fea5a-46df-43a9-b559-d48f2c3cbc38",
+        title: "Chowmein",
+        slug: "chowmein",
+        type: "Short Film",
+        role: "DOP",
+        details: [
+            {
+                type: "VIDEO",
+                videoId: "dF_5rC9Lyu8",
             },
         ],
     },
