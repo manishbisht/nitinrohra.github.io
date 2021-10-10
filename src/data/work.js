c75c329fd92aa84d11287f54a353d24a353c35c9
Nitin Rohra <iamnrohra@gmail.com>
2021-10-11 00:52:59 +0530

    work-addons-withrestructuring

--- src/data/work.js
+++ src/data/work.js
@@ -1,18 +1,5 @@
 export default [
     {
-        id: "0f6280b0-240d-4b35-8d81-2e29585becf7",
-        title: "Noor II",
-        slug: "noor-a-fashion-campaign-two",
-        type: "Ad. Campaign",
-        role: "DOP",
-        details: [
-            {
-                type: "VIDEO",
-                videoId: "cFA6Gx3n-hU",
-            },
-        ],
-    },
-    {
         id: "421ce365-ad3a-4d2a-bdbe-bbb65d3d97ba",
         title: "Noor I",
         slug: "noor-a-fashion-campaign-one",
@@ -22,84 +9,6 @@ export default [
             {
                 type: "VIDEO",
                 videoId: "aCb5V0NZBaI",
-            },
-        ],
-    },
-    {
-        id: "03e7beaa-ee52-48e5-b76d-c999155f4004",
-        title: "Shaam-e-Awadh",
-        slug: "shaam-e-awadh",
-        type: "Event Highlights",
-        role: "DOP",
-        details: [
-            {
-                type: "VIDEO",
-                videoId: "MI0PqYu10jI",
-            },
-        ],
-    },
-    {
-        id: "a5c6c1e3-a4c7-411e-a626-64f5c86324b9",
-        title: "Sach Keh Raha Hai Cover",
-        slug: "sach-keh-raha-hai-cover",
-        type: "Music Video",
-        role: "DOP",
-        details: [
-            {
-                type: "VIDEO",
-                videoId: "MTUzwE_HVSo",
-            },
-        ],
-    },
-    {
-        id: "682a6a28-4ddf-4e63-804f-1314709e2354",
-        title: "Women of 2035",
-        slug: "women-of-2035",
-        type: "Campaign Film",
-        role: "DOP",
-        details: [
-            {
-                type: "VIDEO",
-                videoId: "AMsQM1wXAMk",
-            },
-        ],
-    },
-    {
-        id: "54c06573-378f-4c02-a7bb-aef67011fdbf",
-        title: "WCMT x ISOBAR",
-        slug: "wcmt-x-isobar",
-        type: "Ad. Campaign",
-        role: "DOP",
-        details: [
-            {
-                type: "VIDEO",
-                videoId: "1t7VMGUZuxg",
-            },
-        ],
-    },
-    {
-        id: "3d8394ae-1c4a-4336-bebf-93cafd363b91",
-        title: "The Legend of Jungle",
-        slug: "the-legend-of-jungle",
-        type: "Event Highlights",
-        role: "DOP",
-        details: [
-            {
-                type: "VIDEO",
-                videoId: "Cwmus-2CfZA",
-            },
-        ],
-    },
-    {
-        id: "25809712-ff2d-437f-968c-8ffdffcf5f67",
-        title: "Bubbles & Bowties",
-        slug: "bubbles-and-bowties",
-        type: "Event Highlights",
-        role: "DOP",
-        details: [
-            {
-                type: "VIDEO",
-                videoId: "ao-XiHjzc3s",
             },
         ],
     },
@@ -117,28 +26,54 @@ export default [
         ],
     },
     {
-        id: "41f58500-eee6-4aa6-a04d-3b0a3881b873",
-        title: "Phadphadlal",
-        slug: "phadphadlal",
-        type: "Ad. Film",
-        role: "DOP",
+        id: "682a6a28-4ddf-4e63-804f-1314709e2354",
+        title: "Women of 2035",
+        slug: "women-of-2035",
+        type: "Campaign Film",
+        role: "II unit Cinematographer",
         details: [
             {
                 type: "VIDEO",
-                videoId: "AVsH5UOvisA",
+                videoId: "AMsQM1wXAMk",
             },
         ],
-    },
+    },  
     {
-        id: "aa9739d7-6883-41b8-a811-9352260e23a7",
-        title: "Nut Cracker",
-        slug: "nut-cracker",
+        id: "3d8394ae-1c4a-4336-bebf-93cafd363b91",
+        title: "The Legend of Jungle",
+        slug: "the-legend-of-jungle",
         type: "Event Highlights",
         role: "DOP",
         details: [
             {
                 type: "VIDEO",
-                videoId: "fp7SmYdqTQA",
+                videoId: "Cwmus-2CfZA",
+            },
+        ],
+    },
+    {
+        id: "54c06573-378f-4c02-a7bb-aef67011fdbf",
+        title: "WCMT x ISOBAR",
+        slug: "wcmt-x-isobar",
+        type: "Concert Highlights",
+        role: "DOP",
+        details: [
+            {
+                type: "VIDEO",
+                videoId: "1t7VMGUZuxg",
+            },
+        ],
+    }, 
+    {
+        id: "0f6280b0-240d-4b35-8d81-2e29585becf7",
+        title: "Noor II",
+        slug: "noor-a-fashion-campaign-two",
+        type: "Ad. Campaign",
+        role: "DOP",
+        details: [
+            {
+                type: "VIDEO",
+                videoId: "cFA6Gx3n-hU",
             },
         ],
     },
@@ -156,6 +91,97 @@ export default [
         ],
     },
     {
+        id: "5e30c2a5-99b6-4a75-9836-6ae6df66dbe0",
+        title: "Dream Girl",
+        slug: "dream-girl",
+        type: "Short Film",
+        role: "II Unit Cinematographer",
+        details: [
+            {
+                type: "VIDEO",
+                videoId: "FXhd5z9MsfU",
+            },
+        ],
+    },
+    {
+        id: "41f58500-eee6-4aa6-a04d-3b0a3881b873",
+        title: "Phadphadlal",
+        slug: "phadphadlal",
+        type: "Ad. Film",
+        role: "DOP",
+        details: [
+            {
+                type: "VIDEO",
+                videoId: "AVsH5UOvisA",
+            },
+        ],
+    },
+    {
+        id: "a99cf7db-3566-46d6-859d-85df35418128",
+        title: "MYNTRA x H&M | MOCK AD.",
+        slug: "myntra-x-handm-mock-ad",
+        type: "Mock Ad.",
+        role: "Assistant Cinematographer",
+        details: [
+            {
+                type: "VIDEO",
+                videoId: "856okn-P1Hg",
+            },
+        ],
+    },
+    {
+        id: "03e7beaa-ee52-48e5-b76d-c999155f4004",
+        title: "Shaam-e-Awadh",
+        slug: "shaam-e-awadh",
+        type: "Event Highlights",
+        role: "DOP",
+        details: [
+            {
+                type: "VIDEO",
+                videoId: "MI0PqYu10jI",
+            },
+        ],
+    },
+    {
+        id: "25809712-ff2d-437f-968c-8ffdffcf5f67",
+        title: "Bubbles & Bowties",
+        slug: "bubbles-and-bowties",
+        type: "Event Highlights",
+        role: "DOP",
+        details: [
+            {
+                type: "VIDEO",
+                videoId: "ao-XiHjzc3s",
+            },
+        ],
+    },
+    {
+        id: "aa9739d7-6883-41b8-a811-9352260e23a7",
+        title: "Nut Cracker",
+        slug: "nut-cracker",
+        type: "Event Highlights",
+        role: "DOP",
+        details: [
+            {
+                type: "VIDEO",
+                videoId: "fp7SmYdqTQA",
+            },
+        ],
+    },
+    {
+        id: "a5c6c1e3-a4c7-411e-a626-64f5c86324b9",
+        title: "Sach Keh Raha Hai Cover",
+        slug: "sach-keh-raha-hai-cover",
+        type: "Music Video",
+        role: "DOP",
+        details: [
+            {
+                type: "VIDEO",
+                videoId: "MTUzwE_HVSo",
+            },
+        ],
+    },
+    {
         id: "ee942925-64a4-4481-a8dd-667685e33884",
         title: "Showbuzz",
         slug: "showbuzz",
@@ -169,19 +195,6 @@ export default [
         ],
     },
     {
-        id: "5e30c2a5-99b6-4a75-9836-6ae6df66dbe0",
-        title: "Dream Girl",
-        slug: "dream-girl",
-        type: "Short Film",
-        role: "II Unit Cine.",
-        details: [
-            {
-                type: "VIDEO",
-                videoId: "FXhd5z9MsfU",
-            },
-        ],
-    },
-    {
         id: "418fea5a-46df-43a9-b559-d48f2c3cbc38",
         title: "Chowmein",
         slug: "chowmein",
@@ -191,6 +204,58 @@ export default [
             {
                 type: "VIDEO",
                 videoId: "dF_5rC9Lyu8",
+            },
+        ],
+    },
+    {
+        id: "2c03fa84-fa16-4f1a-a704-9a4027193ce3",
+        title: "Khushi - A Tribute To Mothers",
+        slug: "khushi-a-tribute-to-mothers",
+        type: "Short Film",
+        role: "Sound Recordist",
+        details: [
+            {
+                type: "VIDEO",
+                videoId: "QQ0-5L8rMMI",
+            },
+        ],
+    },
+    {
+        id: "4f8037f1-b434-459c-bd91-dbd4f4f0b908",
+        title: "Nazare",
+        slug: "nazare",
+        type: "Music Video",
+        role: "Assistantt Cinematographer",
+        details: [
+            {
+                type: "VIDEO",
+                videoId: "Zoj6p7cNmi8",
+            },
+        ],
+    },
+    {
+        id: "48580cd2-9b52-4349-a3b6-380d2cadb0b9",
+        title: "SIMONE GOMES Fashion Film",
+        slug: "simone-gomes-fashion-film",
+        type: "Fashion Film",
+        role: "Camera Assistant",
+        details: [
+            {
+                type: "VIDEO",
+                videoId: "SYQ5HGA1OrQ",
+            },
+        ],
+    },
+    {
+        id: "2eca8da1-5a92-466d-8b44-c635d95322e1",
+        title: "CARLOTA COSTA Fashion Film",
+        slug: "carlota-costa-fashion-film",
+        type: "Fashion Film",
+        role: "Camera Assistant",
+        details: [
+            {
+                type: "VIDEO",
+                videoId: "-N74rojaoyQ",
             },
         ],
     },
